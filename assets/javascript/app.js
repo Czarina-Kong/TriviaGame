cb0469bfa91a8c1c9d1e6c5811421ebd22412efa
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2017-11-23 21:38:37 -0800

    pseudocode js

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+//start button begins game
+//start button on click:
+	//hide button
+	//begin timer
+	//populate random question
+	//provide choice answers
+//if player chooses incorrectly or if time remaining = 0
+	//tell player 'you are wrong' or 'out of time'
+	//provide correct answer
+	//insert fun gif
+//if player chooses correctly
+	//tell player 'you are correct'
+	//provide correct answer
+	//insert fun gif
+//at end:
+	//timer stops
+	//message: all done.  here's how you did:
+	//correct answers
+	//incorrect answers
+	//unanswered
+	//start over-->onclick
+		//reset game
+			//reset counters
