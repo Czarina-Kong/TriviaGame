71c4cc79d68d8578877bd233ac43f8225e29c545
zeelee28 <32179557+zeelee28@users.noreply.github.com>
2017-11-27 21:56:51 -0800

    create question and answer arrays

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,33 @@
-//start button begins game
+//let's make some variables
+var triviaQuestion = ["In Aladdin, what is the name of Jasmine's pet tiger?",
+	"In the Lion King, where does Mufasa and his family live?",
+	"In Beauty and the Beast, how many eggs does Gaston eat for breakfast?",
+	"In Mary Poppins, what animal was on the end of Mary Poppins' umbrella that spoke?",
+	"Which of the following is NOT one of the seven Dwarves?",
+	"What was the name of the dragon in Mulan?",
+	"During the ballroom scene of Beauty & the Beast, what color is Belle's gown?",
+	"What name did Princess Aurora use to hide her identity while living in the cottage in the woods?",
+	"What was the crew celebrating on the ship at the beginning of The Little Mermaid?",
+	"Which princess has a raccoon for a sidekick?",
+]
+var triviaAnswer = [['Rajah', 'Jafar', 'Abu', 'Iago'],
+	['Oakland','Pride Cave','Pride Rock','Neverland'],
+	['2 dozen','3 dozen','4 dozen','5 dozen'],
+	['a cat','a parrot','a fox','a lion'],
+	['Happy','Dopey','Angry','Doc'],
+	['Mushu','Li Shang','Yao','Chi-Fu'],
+	['red','white','gold','blue'],
+	['May Showers','Autumn Leaf','Little Red','Briar Rose'],
+	["Prince Eric's Birthday","Prince Eric's Battle Victory","Prince Eric's Engagement", "Prince Eric's Homecoming"],
+	['Jasmine','Snow White','Pocahontas','Rapunzel']
+]
+var correctAnswer = ['Rajah','Pride Rock','5 dozen','a parrot','Angry',
+	'Mushu','gold','Briar Rose',"Prince Eric's Birthday",'Pocahontas']
+var gif = []
+
+
+
+
 //start button on click:
 	//hide button
 	//begin timer
@@ -10,7 +39,6 @@
 	//insert fun gif
 //if player chooses correctly
 	//tell player 'you are correct'
-	//provide correct answer
 	//insert fun gif
 //at end:
 	//timer stops
@@ -21,3 +49,23 @@
 	//start over-->onclick
 		//reset game
 			//reset counters
+
+
+// //Let's make some variables!!
+// var trivia = []
+
+
+// //counters
+// var correctCount == 0
+// var incorrectCount == 0
+// var timer == 0
+
+// //array of Questions and Answers
+
+
+// //booleans
+// var outOfTime == false	//
+// var correct == false	//
+// var gameOver == false	//
+
+// //functions
